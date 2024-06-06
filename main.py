51325e8a409fc476cba5ef544dea6e5f3425eef7
waqasmansoor <waqasmansoor47@yahoo.com>
2024-06-05 21:48:40 -0400

    update main

--- main.py
+++ main.py
@@ -3,35 +3,50 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from utils.dataloader import CreateDataloader
+from utils import imageUtils
+import argparse
 
 batch_size=8
 num_workers=2
 seed=47
 
 if __name__ == '__main__':
-    data_path=Path("C:/Users/PC/Desktop/Masters/Sem 4/COMP 6761/project/data")
-    dir=['restaurant','library','lakeside','golf course','mosques']
 
-    class_one_hot={}
-    for i,classes in enumerate(data_path.glob("*")):
-        class_one_hot[classes.name]=i
-    print("Labels ",class_one_hot)
-
-    images,labels=[],[]
-    for classes in data_path.glob("*"):
-        for c in classes.glob("*"):
-            images.append(c)
-            labels.append(class_one_hot[classes.name])
-    print(f"Total Images: {len(images)}")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', type=str, default='sup', help='sup/unsup')
     
-    train_loader,dataset=CreateDataloader(images,labels,batch_size,shuffle=True,workers=num_workers,seed=seed)
-    start_epoch=0
-    epochs=1
-    nb=len(train_loader)
-    for epoch in range(start_epoch,epochs):
-        for i, (imgs,target) in enumerate(train_loader):
-            for im,l in zip(imgs,target):
-                plt.imshow(im)
-                print(l)
-                plt.show()
-            break
+    opt = parser.parse_args()
+
+
+    data_path=Path("C:/Users/PC/Desktop/Masters/Sem 4/COMP 6761/project/data")
+    dir=['restaurant','library','lakeside','golf course','Auditorium']
+
+    iu=imageUtils(data_path)
+    class_one_hot=iu.oneHotEncoder()
+    print(f"Classes {class_one_hot}")
+    iu.loadDataToRam()
+    
+
+    print(f"Total Images: {len(iu.images)}")
+
+    x_train,x_test,y_train,y_test=iu.split(0.3,0,False,True)
+    if opt.model == "sup":
+        pass
+
+
+    
+
+    """
+        CNN
+    """
+    # train_loader,dataset=CreateDataloader(images,labels,batch_size,shuffle=True,workers=num_workers,seed=seed)
+    # start_epoch=0
+    # epochs=1
+    # nb=len(train_loader)
+    # for epoch in range(start_epoch,epochs):
+    #     for i, (imgs,target) in enumerate(train_loader):
+    #         for im,l in zip(imgs,target):
+    #             plt.imshow(im)
+    #             print(l)
+    #             plt.show()
+    #         break
--- utils/utils.py
+++ utils/utils.py
@@ -1,8 +1,39 @@
 import os
 from pathlib import Path
+from sklearn.model_selection import train_test_split
 
 
 
+
+class  imageUtils:
+    def __init__(self,data_path):
+        self.data_path=data_path
+        self.split_seed=47
+
+    def loadDataToRam(self):
+        self.images,self.labels=[],[]
+        for classes in self.data_path.glob("*"):
+            for c in classes.glob("*"):
+                self.images.append(c)
+                self.labels.append(self.class_one_hot[classes.name])
+
+    def oneHotEncoding(self):
+        self.class_one_hot={}
+        try:
+            for i,classes in enumerate(self.data_path.glob("*")):
+                self.class_one_hot[classes.name]=i
+        except:
+            print("Wrong Path")
+        return self.class_one_hot
+    
+    def split(self,test_size,val_size,val=False,shuffle=True):
+        x_train,x_test,y_train,y_test=train_test_split(self.images,self.labels,shuffle=shuffle,test_size=test_size,random_state=self.split_seed)
+        if val:
+            x_train,x_val,y_train,y_val=train_test_split(x_train,y_train,shuffle=shuffle,test_size=val_size,random_state=self.split_seed)
+            return x_train,x_test,y_train,y_test,x_val,y_val
+        return x_train,x_test,y_train,y_test
+    
+    
 
 
 def rename(classes:list,path:str)->None:
